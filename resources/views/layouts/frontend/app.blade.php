600a2be6f128dc99562e342033b6456c5bb68e23
Bagas Aditya Mahendra <baagas0@gmail.com>
2021-01-12 13:48:41 +0700

    Update 6

--- resources/views/layouts/frontend/app.blade.php
+++ resources/views/layouts/frontend/app.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title>{{ Setting('title')->content }}</title>
-        <meta name="description" content="">
+        <meta name="description" content="Admin | {!! Setting('sejarah')->content !!}">
+        <meta name="keywords" content="{{ Setting('title')->content }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
         <!-- Favicon Icon -->
